86e642d5fb6c2c11f1d5bb496e51260d22a50649
Pascal Ullrich <tentacledev@gmail.com>
2014-01-24 17:45:00 +0100

    Change Show-MergeEnvironment.
    Show merge tool setup. Fix formatting.

--- MergeEnvironment.psm1
+++ MergeEnvironment.psm1
@@ -43,9 +43,11 @@ This Cmdlet simply displays the current setup of the merge environment.
 "Base" usually contains the current customer source code.
 "Source" usually contains the source code which has to be merged into the customer version.
 "Target" will contain the merge output.
+"Merge Tool" shows the path to the executable of the merge tool you are currently using.
 #>
 
   Write-Host "Current merge environment:"
+  Write-Host
   
   Write-Host "Base"
   Write-Host ("-Path: " + (getUserEnvVar $script:ME_BASE))
@@ -62,7 +64,10 @@ This Cmdlet simply displays the current setup of the merge environment.
   Write-Host ("-UserEnvironmentVariable: " + $script:ME_TARGET)
   Write-Host
   
-  #TODO: Add merge tool info
+  Write-Host "Merge Tool" 
+  Write-Host ("-Path: " + (getUserEnvVar $script:ME_TOOL))
+  Write-Host ("-UserEnvironmentVariable: " + $script:ME_TOOL)
+  Write-Host
 }
 
 function Set-MergeEnvironment
