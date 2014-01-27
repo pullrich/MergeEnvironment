d17f943e6c10e9286a8d59c835728d7b91d97a1b
Pascal Ullrich <tentacledev@gmail.com>
2014-01-27 17:05:34 +0100

    Move Set-MergeEnvironment to its own file.

--- PoshMergeMaid.psm1
+++ PoshMergeMaid.psm1
@@ -31,58 +31,7 @@ $modulePath = (Split-Path -parent $MyInvocation.MyCommand.Definition)
 . $modulePath\functions\New-MergeFolder.ps1
 . $modulePath\functions\Show-MergeEnvironment.ps1
 . $modulePath\functions\Clear-MergeTool.ps1
-
-
-
-function Set-MergeEnvironment
-{
-<#
-.SYNOPSIS
-Define three folders for the current merge project.
-#>
-  [CmdletBinding()]
-  param(
-    [Parameter(Mandatory=$false,
-               ValueFromPipeline=$false,
-               Position=0)]
-    [ValidateScript({Test-Path -Path $_ -PathType 'Container'})]
-    [string]
-    $BasePath,
-    
-    [Parameter(Mandatory=$false,
-               ValueFromPipeline=$false,
-               Position=1)]
-    [ValidateScript({Test-Path -Path $_ -PathType 'Container'})]
-    [string]
-    $SourcePath,
-    
-    [Parameter(Mandatory=$false,
-               ValueFromPipeline=$false,
-               Position=2)]
-    [ValidateScript({Test-Path -Path $_ -PathType 'Container'})]
-    [string]
-    $TargetPath
-  )
-  
-  if ($BasePath -ne '')
-  {
-    $BasePath = (Get-Item $BasePath).FullName
-    setUserEnvVar $script:ME_BASE $BasePath
-    Write-Verbose (msgUserEnvVarSetTo $script:ME_BASE)
-  }
-  if ($SourcePath -ne '')
-  {
-    $SourcePath = (Get-Item $SourcePath).FullName
-    setUserEnvVar $script:ME_SOURCE $SourcePath
-    Write-Verbose (msgUserEnvVarSetTo $script:ME_SOURCE)
-  }
-  if ($TargetPath -ne '')
-  {
-    $TargetPath = (Get-Item $TargetPath).FullName
-    setUserEnvVar $script:ME_TARGET $TargetPath
-    Write-Verbose (msgUserEnvVarSetTo $script:ME_TARGET)
-  }
-}
+. $modulePath\functions\Set-MergeEnvironment
 
 function Clear-MergeEnvironment
 {
---
+++ functions/Set-MergeEnvironment.ps1
@@ -0,0 +1,49 @@
+﻿function Set-MergeEnvironment
+{
+<#
+.SYNOPSIS
+Define three folders for the current merge project.
+#>
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory=$false,
+               ValueFromPipeline=$false,
+               Position=0)]
+    [ValidateScript({Test-Path -Path $_ -PathType 'Container'})]
+    [string]
+    $BasePath,
+    
+    [Parameter(Mandatory=$false,
+               ValueFromPipeline=$false,
+               Position=1)]
+    [ValidateScript({Test-Path -Path $_ -PathType 'Container'})]
+    [string]
+    $SourcePath,
+    
+    [Parameter(Mandatory=$false,
+               ValueFromPipeline=$false,
+               Position=2)]
+    [ValidateScript({Test-Path -Path $_ -PathType 'Container'})]
+    [string]
+    $TargetPath
+  )
+  
+  if ($BasePath -ne '')
+  {
+    $BasePath = (Get-Item $BasePath).FullName
+    setUserEnvVar $script:ME_BASE $BasePath
+    Write-Verbose (msgUserEnvVarSetTo $script:ME_BASE)
+  }
+  if ($SourcePath -ne '')
+  {
+    $SourcePath = (Get-Item $SourcePath).FullName
+    setUserEnvVar $script:ME_SOURCE $SourcePath
+    Write-Verbose (msgUserEnvVarSetTo $script:ME_SOURCE)
+  }
+  if ($TargetPath -ne '')
+  {
+    $TargetPath = (Get-Item $TargetPath).FullName
+    setUserEnvVar $script:ME_TARGET $TargetPath
+    Write-Verbose (msgUserEnvVarSetTo $script:ME_TARGET)
+  }
+}
